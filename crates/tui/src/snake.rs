a06e0c83e7163d11083353452cc9e820f7ab4f88
Ilia <istudyatuni@gmail.com>
2024-07-02 23:59:26 +0300

    add comment

--- crates/tui/src/snake.rs
+++ crates/tui/src/snake.rs
@@ -28,6 +28,8 @@ impl Shape for SnakeField {
         let mut paint_point = |pos: Pos, color| {
             let (x, y) = pos.into();
             let x = x * 2;
+            // use two boxes for drawing single cell, because 1 box is looks like
+            // cursor, so 2 boxes is like square
             painter.paint(x, y, color);
             painter.paint(x + 1, y, color);
         };
